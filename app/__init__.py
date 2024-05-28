bf6198a5d3bc277da431239cf0132aedd696ab86
vedsu <90701239+vedsu@users.noreply.github.com>
2024-05-28 12:43:10 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,16 @@ from flask import Flask
 from flask_pymongo import PyMongo
 from flask_cors import CORS
 import boto3, s3fs
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
+connection_string: str = os.environ.get("CONNECTION_STRING")
+access_id: str = os.environ.get("aws_access_key_id")   
+access_key: str = os.environ.get("aws_secret_access_key")  
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"]='mongodb+srv://Vedsu:CVxB6F2N700cQ0qu@cluster0.thbmwqi.mongodb.net/webinar-website'
+app.config["MONGO_URI"]=connection_string
 
 
 
@@ -18,14 +23,14 @@ cors = CORS(app)
 s3_resource = boto3.resource(
     service_name = "s3",
     region_name = 'us-east-1',
-    aws_access_key_id = "AKIA4JNMSYDOLRUH6DHY",
-    aws_secret_access_key = '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'
+    aws_access_key_id = access_id,
+    aws_secret_access_key = access_key
 
 )
 s3_client = boto3.client(
     service_name = "s3",
     region_name = 'us-east-1',
-    aws_access_key_id = "AKIA4JNMSYDOLRUH6DHY",
-    aws_secret_access_key = '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')
+    aws_access_key_id = access_id,
+    aws_secret_access_key =access_key )
 
 from app import routes
