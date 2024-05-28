9a86ca7ca91bf4a18163a461b4d0086e9d1e0a77
vedsu <90701239+vedsu@users.noreply.github.com>
2024-05-28 13:00:37 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,17 +4,17 @@ from flask_pymongo import PyMongo
 from flask_cors import CORS
 import boto3, s3fs
 import os
-from dotenv import load_dotenv
-load_dotenv()
-connection_string: str = os.environ.get("CONNECTION_STRING")
-access_id: str = os.environ.get("aws_access_key_id")   
-access_key: str = os.environ.get("aws_secret_access_key")  
+# from dotenv import load_dotenv
+# load_dotenv()
+# connection_string: str = os.environ.get("CONNECTION_STRING")
+# access_id: str = os.environ.get("aws_access_key_id")   
+# access_key: str = os.environ.get("aws_secret_access_key")  
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"]=connection_string
-
-
+app.config["MONGO_URI"]="mongodb+srv://Vedsu:CVxB6F2N700cQ0qu@cluster0.thbmwqi.mongodb.net/webinar-website"
+aws_access_key_id = "AKIA4JNMSYDOLRUH6DHY"
+aws_secret_access_key = "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"
 
 mongo = PyMongo(app)
 
